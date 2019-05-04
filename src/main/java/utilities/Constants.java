a1d5bed919a2410b4fe3230dddb0f2ca82407c52
Naseer Ahmed <panhwernaseer@gmail.com>
2019-05-04 21:42:45 +0400

    updated code with multibrowser compability

---
+++ src/main/java/utilities/Constants.java
@@ -0,0 +1,8 @@
+package utilities;
+
+public class Constants {
+
+	public static final String browser = "chrome";
+	public static final long implictWait= 30;
+	public static final long pageLoadTimeOut = 30;
+}
--- src/main/java/utilities/SeleniumDriver.java
+++ src/main/java/utilities/SeleniumDriver.java
@@ -1,63 +1,83 @@
 package utilities;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import pages.actions.Navigation;
 
 public class SeleniumDriver {
-	
+
 	private static SeleniumDriver SeleniumDriver;
-	
+
 	private static WebDriver driver;
-	public static WebDriverWait wait ;
-	private final static int pageTimeOut = 10;
-	private final static int implicitlyWait = 30;
+	public static WebDriverWait wait;
 	private final static int timeOut = 10;
 	public static JavascriptExecutor js = null;
-	public static Navigation nav ;
-	
-	
-	private SeleniumDriver()
-	{
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\src\\test\\resources\\executables\\chromedriver.exe");
-		driver = new ChromeDriver();
+	public static Navigation nav;
+
+	private SeleniumDriver() {
+		
+
+		if (Constants.browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+					System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\chromedriver.exe");
+			driver = new ChromeDriver();
+
+			Map<String, Object> prefs = new HashMap<String, Object>();
+			prefs.put("profile.default_content_setting_values.notifications", 2);
+			prefs.put("credentials_enable_service", false);
+			prefs.put("profile.password_manager_enabled", false);
+			ChromeOptions options = new ChromeOptions();
+			options.setExperimentalOption("prefs", prefs);
+			options.addArguments("--disable-extensions");
+			options.addArguments("--disable-infobars");
+
+		} else if (Constants.browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+
+		} else if (Constants.browser.equalsIgnoreCase("ie")) {
+			System.setProperty("webdriver.ie.driver",
+					System.getProperty("user.dir") + "\\src\\test\\resources\\executables\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+
+		}
+
 		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(pageTimeOut, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(implicitlyWait, TimeUnit.SECONDS);
-		wait = new WebDriverWait(driver, timeOut);	
+		driver.manage().timeouts().pageLoadTimeout(Constants.pageLoadTimeOut, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(Constants.implictWait, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, timeOut);
 		nav = new Navigation(driver);
 		js = (JavascriptExecutor) driver;
-	
+
 	}
+
 	
-	public static void openPage(String url)
-	{
+
+	public static void openPage(String url) {
 		driver.get(url);
 	}
-	
-	public static void setupDriver()
-	{
-		if(SeleniumDriver==null)
-		{
+
+	public static void setupDriver() {
+		if (SeleniumDriver == null) {
 			SeleniumDriver = new SeleniumDriver();
 		}
 	}
-	
-	
-	public static WebDriver getDriver()
-	{
+
+	public static WebDriver getDriver() {
 		return driver;
 	}
-	
-	public static void tearDown()
-	{
-		if(SeleniumDriver!= null)
-		{
+
+	public static void tearDown() {
+		if (SeleniumDriver != null) {
 			driver.close();
 			driver.quit();
 			SeleniumDriver = null;
