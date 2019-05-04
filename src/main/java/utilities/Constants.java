379a14e330ef0705761a99803b487e22cadc8ed6
Naseer Ahmed <panhwernaseer@gmail.com>
2019-05-04 22:01:24 +0400

    Updated featuure file and constants

--- src/main/java/utilities/Constants.java
+++ src/main/java/utilities/Constants.java
@@ -2,7 +2,7 @@ package utilities;
 
 public class Constants {
 
-	public static final String browser = "chrome";
+	public static final String browser = "chrome"; //chrome, firefox, ie
 	public static final long implictWait= 30;
 	public static final long pageLoadTimeOut = 30;
 }
